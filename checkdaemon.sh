f5a0e0b3ad9b4091825eea84240586fa5fd78548
postarc <45208608+postarc@users.noreply.github.com>
2018-11-25 20:03:20 +0300

    Create checkdaemon.sh

---
+++ checkdaemon.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# checkdaemon.sh
+# Make sure the daemon is not stuck.
+# Add the following to the crontab (i.e. crontab -e)
+# */30 * * * * ~/condominium/checkdaemon.sh
+
+previousBlock=$(cat ~/condominium/blockcount)
+currentBlock=$(condominium-cli $1 $2 getblockcount)
+
+condominium-cli $1 $2 getblockcount > ~/condominium/blockcount
+
+if [ "$previousBlock" == "$currentBlock" ]; then
+  condominium $1 $2 stop
+  sleep 5
+  condominiumd -daemon $1 $2 
+fi 
